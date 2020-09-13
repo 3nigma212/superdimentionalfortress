8126a6fe45557b11a52ad8056b71f5ddc5c86816
Daniel Xu <danielthepig333@gmail.com>
2020-09-13 15:22:31 -0700

    Finished the status

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ const client = new Discord.Client();
 
 client.on("ready", () =>{
     console.log(`Ready to comply.`);
-    bot.user.setActivity('Need more developers and beta testers, please sign up by DMing Daniel.');
+    bot.user.setActivity('Need more developers and beta testers, please sign up by DMing Daniel.' {type: 'STREAMING'}).catch(console.error);
  
  });
  
